36b790584a381de63119f9a09234990eaff553d8
James Bomediano <kristoffer_bomediano@commude.ph>
2024-10-02 16:59:53 +0800

    [update] directory

--- portfolio__jammmeess/src/components/Home.jsx
+++ portfolio__jammmeess/src/components/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 import About from "./About";
 import Hero from "./Hero";
 import Skills from "./Skills";
-// import Experience from "./Experience";
+import Experience from "./Experience";
 // import Contact from "./Contact";
 // import Footer from "./Footer";
 // import Projects from "./Project";
@@ -20,7 +20,7 @@ const Home = () => {
         <Hero />
         <About />
         <Skills />
-        {/* <Experience /> */}
+        <Experience />
         {/* <Projects /> */}
         {/* <Contact /> */}
       </main>

7815ce997336e02766ac87ef6cc90e9590d2b501
James Bomediano <kristoffer_bomediano@commude.ph>
2024-10-02 17:01:31 +0800

    [add] experience section

---
+++ portfolio__jammmeess/src/components/Experience.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <section className="expSec" id="Experience">
+          <div className="expSec__inner">
+            <div className="expSec__ttlCont">
+              <h2 className="expSec__ttl">_work<span className="txtBlue">Experience.()</span></h2>
+            </div>
+            <div className="expSec__wrkWrapper">
+              <div className="expSec__wrkCont">
+                <h3 className="expSec__wrkTtl">Commude Ph</h3>
+                <p className="expSec__wrkPos">Jr. Front-End <br/>Web Developer</p>
+                <p className="expSec__wrkDate">May 2024 <span className="txtBlue">-</span> Present</p>
+              </div>
+              <div className="expSec__wrkCont">
+                <h3 className="expSec__wrkTtl">De Castro Consulting</h3>
+                <p className="expSec__wrkPos">Web Developer<br/> Intern</p>
+                <p className="expSec__wrkDate">January 2024 <span className="txtBlue">-</span> Aprilt</p>
+              </div>
+              <div className="expSec__wrkCont">
+                <h3 className="expSec__wrkTtl">Costa Cruises</h3>
+                <p className="expSec__wrkPos">Demi Chef <br/>De Partie</p>
+                <p className="expSec__wrkDate">March 2018 <span className="txtBlue">-</span> March 2023</p>
+              </div>
+            </div>
+          </div>
+        </section>
+    );
+};
+
+export default Experience;

7361c7e8898488066d23852c6648b4379b32050c
James Bomediano <kristoffer_bomediano@commude.ph>
2024-10-03 17:38:15 +0800

    [fix] minor fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import navIcon from "../img/navbar/menu.png";
+import Resume from "../files/Bomediano_Resume.pdf"
 import Logo from "../img/navbar/portfoliologo.png";
 
 const Navbar = () => {
@@ -28,12 +29,19 @@ const Navbar = () => {
                 <div className="navbar__navCont">
                     <div className="navbar__navInner">
                         <img className="navbar__navLogo" src={Logo} alt="Site Logo" />
-                        <ul className="navbar__navList">
-                            <li className="navbar__navListItem"><a className="navbar__navLink" href="#About">about.me()</a></li>
-                            <li className="navbar__navListItem"><a className="navbar__navLink" href="#Experience">my.experience()</a></li>
-                            <li className="navbar__navListItem"><a className="navbar__navLink" href="#Projects">my.projects()</a></li>
-                            <li className="navbar__navListItem"><a className="navbar__navLink" href="#Contact">contact.me()</a></li>
-                        </ul>
+                        <div className="navbar__linkWrap">
+                            <ul className="navbar__navList">
+                                <li className="navbar__navListItem"><a className="navbar__navLink" href="#About">about.me()</a></li>
+                                <li className="navbar__navListItem"><a className="navbar__navLink" href="#Experience">my.experience()</a></li>
+                                <li className="navbar__navListItem"><a className="navbar__navLink" href="#Projects">my.projects()</a></li>
+                                <li className="navbar__navListItem"><a className="navbar__navLink" href="#Contact">contact.me()</a></li>
+                            </ul>
+                            {/* <a href={Resume} download >
+                                <button className="commonBtn__primaryBtn navBtn">
+                                    Download Resume
+                                </button> */}
+                            {/* </a> */}
+                        </div>
                     </div>
                 </div>
             </nav>

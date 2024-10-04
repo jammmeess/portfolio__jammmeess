ca6c7be942843988d13acfbbb629b87664f45a3f
James Bomediano <kristoffer_bomediano@commude.ph>
2024-10-04 18:23:14 +0800

    revisions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
-import SCSS from "./index.scss";
+import "./index.scss";
 
 function App() {
   return (

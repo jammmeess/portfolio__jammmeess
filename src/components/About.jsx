23f4a25d573437f024fb2873d083a0ebaefa7400
James Bomediano <kristoffer_bomediano@commude.ph>
2024-11-06 10:06:41 +0800

    [add] Title animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Profile from "../img/about/profile__picture.jpeg";
 import Arrow from "../img/common/arrow.png";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+    const aboutTitleRef = useRef(null);
+    const aboutLwrTitleRef = useRef(null);
+
+    useEffect(() => {
+        gsap.fromTo(
+            aboutTitleRef.current, 
+            {
+                y: 200,
+                opacity: 0,
+            },
+            {
+                y: -20,
+                opacity: 1,
+                duration: 2,
+                scrollTrigger: {
+                    trigger: aboutTitleRef.current,
+                    start: "top 100%",
+                    toggleActions: "play none none none",
+                }
+            }
+        );
+
+        gsap.fromTo(
+            aboutLwrTitleRef.current, 
+            {
+                y: 200,
+                opacity: 0,
+                color: "#E7ECEF",
+            },
+            {
+                y: -20,
+                opacity: 1,
+                delay: 1,
+                duration: 2,
+                color: "#0047AB",
+                scrollTrigger: {
+                    trigger: aboutLwrTitleRef.current,
+                    start: "top 100%",
+                    toggleActions: "play none none none",
+                }
+            }
+        );
+    }, []);
+
     return (
         <section className="aboutSec" id="About">
             <div className="aboutSec__inner">
                 <div className="aboutSec__contntWrap">
-                    <h2 className="aboutSec__ttl">FRONT-END <br/><span className="txtBlue">WEB DEVELOPER</span>
+                    <h2 className="aboutSec__ttl" ref={aboutTitleRef}>
+                        FRONT-END <br/>
+                        <span className="txtBlue" ref={aboutLwrTitleRef}>WEB DEVELOPER</span>
                     </h2>
                     <figure className="aboutSec__imgWrap">
                         <img src={Profile} alt=""/>
@@ -20,8 +70,9 @@ const About = () => {
                 <div className="aboutSec__lowerContntWrap">
                     <div className="aboutSec__txtWrap">
                         <p className="aboutSec__txt">
-                        I am a front-end web developer based on the Philippines. I really enjoy working in web development, especially when it comes to exploring different technologies. I find it easy to manage my own projects and love collaborating with others as part of a team.
-                        <br/> <br/>I'm always eager to learn and stay updated with the latest industry trends. This helps me bring fresh and innovative solutions to my work, keeping things exciting and effective.
+                            I am a front-end web developer based on the Philippines. I really enjoy working in web development, especially when it comes to exploring different technologies. I find it easy to manage my own projects and love collaborating with others as part of a team.
+                            <br/><br/>
+                            I'm always eager to learn and stay updated with the latest industry trends. This helps me bring fresh and innovative solutions to my work, keeping things exciting and effective.
                         </p>
                     </div>
                     <div className="aboutSec__ttlWrap">
